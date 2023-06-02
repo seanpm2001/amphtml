3f7f7da751ff6cfccb60f0a7e913640f88c17230
Daniel Rozenberg <rodaniel@amp.dev>
2023-06-02 16:41:59 -0400

    Regenerate Babel plugin tests

--- build-system/babel-plugins/babel-plugin-nomodule-loader/test/fixtures/transform/only-exports/output.js
+++ build-system/babel-plugins/babel-plugin-nomodule-loader/test/fixtures/transform/only-exports/output.js
@@ -4,16 +4,13 @@
     defineCallback.apply(null, arguments);
     var name = "build-system/babel-plugins/babel-plugin-nomodule-loader/test/fixtures/transform/only-exports/input";
     var awaiting = callbacks[name] = callbacks[name] || [];
-
     while (awaiting.length) {
       awaiting.pop()(_exports);
     }
-
     awaiting.push = function (callback) {
       callback(_exports);
     };
   } : defineCallback;
-
   if (true) {
     exec({});
   } else if (null) {
@@ -24,7 +21,6 @@
       if (true && name === 0) {
         return {};
       }
-
       return new Promise(function (resolve) {
         (callbacks[name] = callbacks[name] || []).push(resolve);
       });
@@ -36,10 +32,9 @@
   "use strict";
 
   _exports.__esModule = true;
-  _exports.y = y;
   _exports.x = void 0;
+  _exports.y = y;
   const x = 'y';
   _exports.x = x;
-
   function y() {}
 });
